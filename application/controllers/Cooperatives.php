0f4fdf88ccc8535c3eec36937c9bf19d9b375370
Json <jaysondelapaz16@gmail.com>
2020-05-26 16:03:27 +0000

    Update Cooperatives.php

--- application/controllers/Cooperatives.php
+++ application/controllers/Cooperatives.php
@@ -194,9 +194,11 @@
               $data['coop_type'] = $this->cooperatives_model->get_type_of_coop($data['coop_info']->type_of_cooperative);
               $count=0;
               $data['document_others1'] = $this->uploaded_document_model->get_document_one_info($decoded_id);
-             
+              $data['staff_complete'] = $this->staff_model->requirements_complete($decoded_id);
+              if($data['staff_complete'])
+              {
                 foreach ($data['coop_type'] as $coop) : 
-                    print_r($coop);
+                    
                     $count++;
                     if($count == 1){
                         $data['document_others1'] = $this->uploaded_document_model->get_document_others1_info($decoded_id,$coop['document_num']);
@@ -208,6 +210,7 @@
                     }
                     
                 endforeach;
+              }    
               
               
                 
