e10abc635bfc805bf651005c87337629e231680d
Json <jaysondelapaz16@gmail.com>
2020-06-05 10:51:08 +0000

    Update list_of_documents.php

--- application/views/documents/list_of_documents.php
+++ application/views/documents/list_of_documents.php
@@ -355,13 +355,13 @@
   </div> -->
 </div>
 <!--ANJURY-->
-<div class="row">cccccccccccccccccccc
+<div class="row">
 <?php 
 // print_r($ching);
 // echo"<pre>";print_r($coop_type);echo"<pre>";
 $count=0;
     foreach ($coop_type as $coop) : 
-        echo"<pre>";print_r($coop);echo"<pre>";
+      
 ?>
     <?php $count++;?>
     <div class="col-sm-12 col-md-4">
