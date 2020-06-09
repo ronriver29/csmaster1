d9ac9f9640d0072f1d0871ae04191f7875ffd7a0
Kelvin Joaquin <adiktus9116@yahoo.com>
2020-06-09 15:57:05 +0800

    add folder for qrcodes

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
 cp -r /srv/site/. /var/www/html
 [ -d /var/www/shared-volume/amendment ] || mkdir /var/www/shared-volume/amendment
+[ -d /var/www/shared-volume/qrcodes ] || mkdir /var/www/shared-volume/qrcodes
