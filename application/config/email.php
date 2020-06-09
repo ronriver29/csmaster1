0fa67a729531808d8f1170ac8effd1c862ade7ec
jaysondelapaz16@gmail.com <jaysondelapaz16@gmail.com>
2020-06-10 00:27:47 +0800

    try php open ssl

--- application/config/email.php
+++ application/config/email.php
@@ -1,24 +1,24 @@
 <?php
-// $config['protocol'] = 'smtp';
-// $config['smtp_host'] = 'ssl://smtp.gmail.com';
-// $config['smtp_port'] = '465';
-// $config['smtp_user'] = 'cooperative.testing01@gmail.com';
-// $config['smtp_pass'] = 'kamote]]';  //sender's password
-// $config['mailtype'] = 'html';
-// $config['charset'] = 'iso-8859-1';
-// $config['wordwrap'] = 'TRUE';
-// $config['newline'] = "\r\n";
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'ssl://smtp.gmail.com';
+$config['smtp_port'] = '465';
+$config['smtp_user'] = 'cooperative.testing01@gmail.com';
+$config['smtp_pass'] = 'kamote]]';  //sender's password
+$config['mailtype'] = 'html';
+$config['charset'] = 'iso-8859-1';
+$config['wordwrap'] = 'TRUE';
+$config['newline'] = "\r\n";
 
-$config = Array(
-    'protocol' => 'smtp',
-    'smtp_host' => 'tls://smtp.gmail.com',
-    'smtp_port' => 587,
-    'smtp_user' => 'cooperative.testing01@gmail.com',
-    'smtp_pass' => 'kamote]]',
-    'mailtype'  => 'html',
-    'charset'   => 'iso-8859-1',
-    'wordwrap' => 'TRUE',
-    'newline' => "\r\n"
-);
+// $config = Array(
+//     'protocol' => 'smtp',
+//     'smtp_host' => 'ssl://smtp.googlemail.com',
+//     'smtp_port' => 465,
+//     'smtp_user' => 'cooperative.testing01@gmail.com',
+//     'smtp_pass' => 'kamote]]',
+//     'mailtype'  => 'html',
+//     'charset'   => 'iso-8859-1',
+//     'wordwrap' => 'TRUE',
+//     'newline' => "\r\n"
+// );
 
 ?>
