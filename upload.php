8f9130ab8021ef61cd28b8b5b169c5525150160a
Kelvin Joaquin <adiktus9116@yahoo.com>
2020-08-20 08:57:59 +0800

    add upload tester

---
+++ upload.php
@@ -0,0 +1,20 @@
+<?php
+
+$target_dir = "/tmp";
+$uploaded = true;
+
+if (isset($_POST["submit"])) {
+    $target_file = $target_dir . basename($_FILES["upload"]["name"]);
+    if (move_uploaded_file($_FILES["upload"]["tmp_name"], $target_file)) {
+        echo "<br/>Uploaded to ". $target_file. " with size of " .$_FILES["upload"]["size"]. "<br/>";
+    }
+    
+}
+
+?>
+
+<form method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="upload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
