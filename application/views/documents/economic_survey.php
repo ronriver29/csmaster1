a4cc6c557ba053a2276b895131f3adb36ee41670
jaysondelapaz16@gmail.com <jaysondelapaz16@gmail.com>
2020-11-04 19:19:15 +0800

    fix economic survey3

--- application/views/documents/economic_survey.php
+++ application/views/documents/economic_survey.php
@@ -179,6 +179,7 @@
               <td><u><?php echo($bylaw_info->kinds_of_members == 1) ? number_format(($total_regular['total_subscribed'] * $capitalization_info->par_value),2) : number_format((($total_regular['total_subscribed'] * $capitalization_info->par_value) + ($total_associate['total_subscribed'] * $capitalization_info->par_value)),2);?></u></td>
             </tr>
             <tr>
+
               <td>Paid-up Capital :</td>
               <td><u><?php echo($bylaw_info->kinds_of_members == 1) ? number_format(($total_regular['total_paid'] * $capitalization_info->par_value),2) : number_format((($total_regular['total_paid'] * $capitalization_info->par_value) + ($total_associate['total_paid'] * $capitalization_info->par_value)),2);?></u></td>
             </tr>
@@ -261,7 +262,7 @@
               <p class="text-justify font-weight-normal"><span style="font-family: DejaVu Sans; sans-serif;"><?php echo ($survey_info->generate_capital[4] == "1") ? "X - ": ""?></span>Fund raising activities</p>
             </li>
             <li>
-               How much is the Cooperative’s initial operating capital? <u><?=number_format($capitalization_info->total_amount_of_paid_up_capital,2)?></u>.
+               How much is the Cooperative’s initial operating capital? <u><?=number_format($survey_info->amount_initial_operating_capital,2)?></u>.
             </li>
             <li>
               Strategies for internal capital build-up. <u><?=$survey_info->strategy_capital_build_up?></u>.
