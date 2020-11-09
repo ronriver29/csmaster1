a62a8b95124022c81507d281ab4dd78d63444b09
Anjury Patawaran <anjurypatawaran@gmail.com>
2020-11-09 06:15:09 +0000

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -183,7 +183,7 @@ $config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-@\='; // default value 'a-z 0-9~
 | use segment based URLs.
 |
 */
-$config['enable_query_strings'] = FALSE;
+$config['enable_query_strings'] = TRUE;
 $config['controller_trigger'] = 'c';
 $config['function_trigger'] = 'm';
 $config['directory_trigger'] = 'd';
