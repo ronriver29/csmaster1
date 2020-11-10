09c635ad39882eed83a555742e6f8a2c8c0a995d
jaysondelapaz16@gmail.com <jaysondelapaz16@gmail.com>
2020-11-10 18:32:57 +0800

    added cais migration table

---
+++ application/migrations/054_modify_capris_table_cais.php
@@ -0,0 +1,32 @@
+<?php
+class Migration_modify_capris_table_cais extends CI_Migration
+{
+    public function up()
+    {
+        $this->dbforge->add_field(
+           $field = array
+                  (
+                     'other_officer'=>array(
+                      'type'=>'INT',
+                      'constraint' =>11,
+                      'null' => TRUE
+                    )
+                  )
+        );
+        $this->dbforge->add_column('capris',$field);
+    }
+
+    public function down()
+    { 
+        $result = $this->db->query("SHOW COLUMNS FROM `capris` LIKE 'other_officer'");
+        // $exists = ($result)?TRUE:FALSE;
+        if($result->num_rows()>0)
+        {
+           $qry = $this->db->query("ALTER TABLE capris DROP COLUMN other_officer"); 
+        }
+
+     
+      
+    }
+}
+?>
