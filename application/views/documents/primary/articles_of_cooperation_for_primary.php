d27c52b5a265415850e4d49f4164939d0752f88a
jaysondelapaz16@gmail.com <jaysondelapaz16@gmail.com>
2020-12-07 15:42:25 +0800

    modified articles of cooperation doc

--- application/views/documents/primary/articles_of_cooperation_for_primary.php
+++ application/views/documents/primary/articles_of_cooperation_for_primary.php
@@ -286,7 +286,7 @@
   <div class="row mb-4">
     <div class="col-sm-12 col-md-12">
       <ol class="text-justify" type="a">
-        <li> <?= ucwords(num_format_custom($capitalization_info->common_share))?> Pesos (<?= number_format($capitalization_info->common_share,2)?>) common shares with a par value of <?= ucwords(num_format_custom($capitalization_info->par_value))?> Pesos (<?=number_format($capitalization_info->par_value,2)?> ) per share;</li>
+        <li> <?= ucwords(num_format_custom($capitalization_info->common_share))?> (<?= number_format($capitalization_info->common_share)?>) common shares with a par value of <?= ucwords(num_format_custom($capitalization_info->par_value))?> Pesos (<?=number_format($capitalization_info->par_value,2)?> ) per share;</li>
         <?php if($bylaw_info->kinds_of_members == 2) :?>
         <li> <?= ucwords(num_format_custom($capitalization_info->preferred_share))?> Pesos (<?= number_format($capitalization_info->preferred_share,2)?>) preferred shares with a par value of <?= ucwords(num_format_custom($capitalization_info->par_value))?> (<?=$capitalization_info->par_value?> ) per share.</li>
         <?php endif;?>
