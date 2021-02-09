6259f34b57472cad0ea7b868c1ec5ef2f7824fe6
jaysondelapaz16@gmail.com <jaysondelapaz16@gmail.com>
2021-02-09 09:25:58 +0800

    update js val

--- assets/js/jquery.validationEngine-en.js
+++ assets/js/jquery.validationEngine-en.js
@@ -353,14 +353,24 @@
                     "alertTextLoad": "* Validating, please wait"
                 },
                  "ajaxMinimumAssociateSubscriptionCallPhpAmendment": {
-                    "url": "../../../../amendmentbylaws/check_minimum_associate_subscription",
+                    "url": "../../amendmentbylaws/check_minimum_associate_subscription",
+                     "url": "../../../amendmentbylaws/check_minimum_associate_subscription",
+                     "url": "../../../../amendmentbylaws/check_minimum_associate_subscription",
                     // you may want to pass extra data on the ajax call
-                    "extraDataDynamic": ['#amd_id'],
+                    "extraDataDynamic": ['#cooperatorID,#cooperative_id,#amd_id'],
                     "alertText": "* Must be greater than or equal to the minimum associate subscription indicated in the bylaws and Must not be greater than 10% of your Total no of paid-up capital",
                     "alertTextOk": "* This is greater than or equal to the minimum associate subscription indicated in the bylaws",
                     "alertTextLoad": "* Validating, please wait"
                 },
-
+                    //pay up share associate
+                "ajaxMinimumAssociatePayEditAmendmentCallPhp": {
+                    "url": "../../amendmentbylaws/check_minimum_associate_pay_amendment",
+                    // you may want to pass extra data on the ajax call
+                    "extraDataDynamic": ['#cooperatorID,#cooperative_id,#amd_id'],
+                    "alertText": "* Must be greater than or equal to the minimum associate pay indicated in the bylaws",
+                    "alertTextOk": "* This is greater than or equal to the minimum associate pay subscription indicated in the bylaws",
+                    "alertTextLoad": "* Validating, please wait"
+                },
                 "ajaxMinimumAssociatePayCallPhp": {
                     "url": "../../bylaws/check_minimum_associate_pay",
                     // you may want to pass extra data on the ajax call
@@ -407,16 +417,6 @@
                     "url": "../../../../bylaws/check_minimum_associate_pay",
                     // you may want to pass extra data on the ajax call
                     "extraDataDynamic": ['#cooperativesID'],
-                    "alertText": "* Must be greater than or equal to the minimum associate pay indicated in the bylaws",
-                    "alertTextOk": "* This is greater than or equal to the minimum associate pay subscription indicated in the bylaws",
-                    "alertTextLoad": "* Validating, please wait"
-                },
-                //pay up share associate
-                "ajaxMinimumAssociatePayEditAmendmentCallPhp": {
-                    "url": "../../amendmentbylaws/check_minimum_associate_pay_amendment",
-                 
-                    // you may want to pass extra data on the ajax call
-                    "extraDataDynamic": ['#cooperatorID,#cooperative_id,#amd_id'],
                     "alertText": "* Must be greater than or equal to the minimum associate pay indicated in the bylaws",
                     "alertTextOk": "* This is greater than or equal to the minimum associate pay subscription indicated in the bylaws",
                     "alertTextLoad": "* Validating, please wait"
