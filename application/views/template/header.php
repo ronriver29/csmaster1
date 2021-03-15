d94bf789f43330fbe99cb671a11335af025bc340
jaysondelapaz16@gmail.com <jaysondelapaz16@gmail.com>
2021-03-15 15:24:04 +0800

    retry last commit

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -51,11 +51,9 @@
         <li>
           <a href="<?php echo base_url();?>branches"><i class="fas fa-handshake"></i> Branches and Satellites</a>
         </li>
-       
         <li>   
           <a href="<?php echo base_url();?>laboratories"><i class="fas fa-handshake"></i> Laboratories</a>
         </li>
-
        <li>   
           <a href="<?php echo base_url();?>amendment"><i class="fas fa-handshake"></i> Amendment</a>
         </li> 
