29ea28d848515b743e7404607fa02fa65ac93180
jaysondelapaz16@gmail.com <jaysondelapaz16@gmail.com>
2021-04-20 14:13:16 +0800

    update migration

--- application/controllers/Amendment_upload.php
+++ application/controllers/Amendment_upload.php
@@ -67,23 +67,24 @@ public function importFile(){
 					$flag =false;
 					continue;
 					}
-					if(strlen($value['F'])>0)
+					if(strlen($value['B'])>0)
 					{	
 						$amendment_id++;
-						$inserdata[$i]['id'] = $amendment_id;
-						$inserdata[$i]['regNo'] = $value['C'];
-						$inserdata[$i]['amendmentNo']=1;
+						$inserdata[$i]['id'] = $value['A'];
+						$inserdata[$i]['regNo'] = $value['B'];
+						$inserdata[$i]['amendmentNo']=$value['C'];
 						$inserdata[$i]['users_id']=null;
-						$inserdata[$i]['category_of_cooperative'] = $value['D'];
-						$inserdata[$i]['type_of_cooperative']= $value['E'];
+						$inserdata[$i]['category_of_cooperative'] = $value['E'];
+						$inserdata[$i]['type_of_cooperative']= $value['F'];
 						$inserdata[$i]['cooperative_type_id']=null;
 						$inserdata[$i]['grouping']=null;
-						$inserdata[$i]['proposed_name'] =$value['F'];
+						$inserdata[$i]['proposed_name'] =$value['D'];
 						$inserdata[$i]['acronym']=$value['G'];
-						$inserdata[$i]['common_bond_of_membership']= $value['K'];
+						$inserdata[$i]['common_bond_of_membership']= $value['J'];
+						$inserdata[$i]['comp_of_membership'] = $value['K'];
 						$inserdata[$i]['field_of_membership']= $value['L'];
 						$inserdata[$i]['name_of_ins_assoc']=$value['M'];
-						$type = explode(',',$value['E']);
+						$type = explode(',',$value['F']);
 						$final_type = 'Single';
 						if(count($type)>1)
 						{
@@ -95,7 +96,25 @@ public function importFile(){
 						$inserdata[$i]['street']=$value['Q'];
 						$inserdata[$i]['house_blk_no']=$value['P'];
 						$inserdata[$i]['status']=1;
-						
+
+						$insertdataReg[$i]['coopName'] = $value['D'];
+						$insertdataReg[$i]['acronym'] = $value['G'];
+						$insertdataReg[$i]['regNo'] = $value['B'];
+						$insertdataReg[$i]['category'] = $value['E'];
+						$insertdataReg[$i]['type'] = $value['F'];
+						$insertdataReg[$i]['dateRegistered'] = $value['R'];
+						$insertdataReg[$i]['commonBond'] = $value['J'];
+						$insertdataReg[$i]['areaOfOperation'] = $value['N'];
+						$insertdataReg[$i]['noStreet'] = $value['P'];
+						$insertdataReg[$i]['Street'] = $value['P'];
+						$insertdataReg[$i]['addrCode'] = $value['O'];
+						$insertdataReg[$i]['amendment_id'] = $value['A'];
+						$insertdataReg[$i]['addrCode'] = $value['O'];
+						$insertdataReg[$i]['addrCode'] = 0;
+
+
+
+
 						//major industry
 						if(strlen($value['H'])>0)
 						{
@@ -104,40 +123,51 @@ public function importFile(){
 							{
 								foreach($q->result() as $qrow)
 								{
-									// $this->debug($qrow->id.': '.$qrow->description);
-									$data_business_activity = array(
-										'major_industry_id'=>$qrow->id
-									);	
+									$major_industry_id = $qrow->id;
 								}
-							}
-							//subclass
-							$qs = $this->db->get_where('subclass',array('description'=>$value['I']));
-							if($qs->num_rows()>0)
-							{
-								foreach($qs->result() as $srow)
+
+
+								$qs = $this->db->get_where('subclass',array('description'=>$value['I']));
+								if($qs->num_rows()>0)
 								{
-									$data_business_activity_subclass = array(
-										'subclass_id'=>$srow->id
-									);
-									// $this->debug($srow->id.': '.$srow->description);
+									foreach($qs->result() as $srow)
+									{
+										$subclass_id = $srow->id;
+										
+									}
 								}
+
 							}
-							// $this->debug($this->db->last_query());
+							 $business_data=  array(
+							 	'cooperatives_id' => 0,
+							 	'amendment_id' => $value['A'],
+							 	'major_industry_id' => $major_industry_id,
+							 	'subclass_id' => $subclass_id
+							 );
+						}
+						// $this->debug($business_data);
+						if(!$this->db->insert('business_activities_cooperative_amendment',$business_data))
+						{
+							echo "failed to insert business activity Amendment data";
 						}
 					}//end if		
 					
 					// $this->debug('amendment_id :'. $amendment_id);
 					$i++;
 				}      
-					$this->debug($inserdata);
-					// $this->db->insertBatch('amend_coop')         
-					// $result = $this->import->insert($inserdata);   
-					// // // echo $this->db->last_query();
-					// if($result){
-					// echo "Imported successfully";
-					// }else{
-					// echo "ERROR !";
-					// }             
+					// $this->debug($inserdata);
+					// $this->db->insertBatch('amend_coop') 
+					if($this->db->insert_batch('registeredcoop',$insertdataReg))
+					{
+						$result = $this->import->insert($inserdata);   
+					// // echo $this->db->last_query();
+						if($result){
+						echo "Imported successfully";
+						}else{
+						echo "ERROR !";
+						}       
+					}        
+					      
 			} 
 			catch (Exception $e) 
 			{
@@ -151,7 +181,7 @@ public function importFile(){
 		}
 	}//end of submit
 		$this->load->view('amendment/v_upload');
-} //end of public
+   } //end of public
 	
 	// public function get_coop_id($regNo)
 	// {
@@ -162,6 +192,17 @@ public function importFile(){
         echo"<pre>";
         print_r($array);
         echo"</pre>";
+    }
+    public function update_migration($version)
+    {
+    	if($this->db->query("UPDATE migrations SET version='$version'"))
+    	{
+    		echo"success";
+    	}
+    	else
+    	{
+    		echo"failed";
+    	}
     }
 }
 ?>
