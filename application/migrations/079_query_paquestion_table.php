c02413a2f545b83840c3fe29decf4a7b3252e52a
jaysondelapaz16@gmail.com <jaysondelapaz16@gmail.com>
2021-04-30 09:52:25 +0800

    migration alter query paquestion

---
+++ application/migrations/079_query_paquestion_table.php
@@ -0,0 +1,14 @@
+<?php
+class Migration_query_paquestion_table extends CI_Migration
+{
+    public function up()
+    {
+       $this->db->query("UPDATE `paquestion` SET `isScorable` = 1, `score` = 2 WHERE `num` = 131");
+    }
+
+    public function down()
+    { 
+       $this->db->query("UPDATE `paquestion` SET `isScorable` = 0, `score` = 5 WHERE `num` = 131");
+    }
+}
+?>
