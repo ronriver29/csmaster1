1bc10395c81d41cb2ce23b7ae21fadee366bba83
jaysondelapaz16@gmail.com <jaysondelapaz16@gmail.com>
2021-05-06 11:45:10 +0800

    update industry_subclass_model.php

--- application/models/Industry_subclass_model.php
+++ application/models/Industry_subclass_model.php
@@ -21,5 +21,16 @@ class Industry_Subclass_model extends CI_Model{
     $query = $this->db->get();
     return $query->result_array();
   }
-
+  public function get_industry_subclasses_amendmnet($major_industry_id){
+    $this->db->order_by('description', 'ASC');
+    $this->db->distinct();
+    $this->db->select('subclass.id,subclass.description');
+    $this->db->from('industry_subclass_by_coop_type');
+    $this->db->join('major_industry', 'major_industry.id = industry_subclass_by_coop_type.major_industry_id');
+    $this->db->join('subclass', 'subclass.id = industry_subclass_by_coop_type.subclass_id');
+    $this->db->where(array('major_industry.id'=>$major_industry_id));
+    $query = $this->db->get();
+    return $query->result_array();
+  }
+ 
 }
