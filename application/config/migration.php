7aa73f021d709d74eca862d3ac4003c374b4d720
jaysondelapaz16@gmail.com <jaysondelapaz16@gmail.com>
2021-05-18 10:08:53 +0800

    update migration.php

--- application/config/migration.php
+++ application/config/migration.php
@@ -71,7 +71,7 @@ $config['migration_auto_latest'] = TRUE;
 */
 
 // <<<<<<< HEAD
-$config['migration_version'] = 85;
+$config['migration_version'] = 87;
 // =======
 // $config['migration_version'] = 84;
 // >>>>>>> da8e723e250548a466d7fe2b38fb67deddfe8307
