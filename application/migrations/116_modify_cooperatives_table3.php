9f172a6d30641efabb488b63dbd068252de98fc3
jaysondelapaz16@gmail.com <jaysondelapaz16@gmail.com>
2021-10-07 14:02:57 +0800

    update migration

---
+++ application/migrations/116_modify_cooperatives_table3.php
@@ -0,0 +1,32 @@
+<?php
+class Migration_modify_cooperatives_table extends CI_Migration
+{
+    public function up()
+    {
+        $this->dbforge->add_field(
+           $field = array
+                  (
+                     'capital_contribution'=>array(
+                      'type'=>'INT',
+                      'constraint'=>11,
+                      'after' => 'temp_evaluation_comment',
+                      // 'null' => TRUE
+                      'default' => 0
+                    )
+                  )
+        );
+        $this->dbforge->add_column('cooperatives',$field);
+    }
+
+    public function down()
+    { 
+        $result = $this->db->query("SHOW COLUMNS FROM `cooperatives` LIKE 'capital_contribution'");
+        // $exists = ($result)?TRUE:FALSE;
+        if($result->num_rows()>0)
+        {
+           $qry = $this->db->query("ALTER TABLE cooperatives DROP COLUMN capital_contribution"); 
+        }
+
+    }
+}
+?>
