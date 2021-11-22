d4e0bf1fae687a95c0203015cd37cd4ccb3c6ddd
jaysondelapaz16@gmail.com <jaysondelapaz16@gmail.com>
2021-11-22 18:20:37 +0800

    update migration 127

--- application/migrations/127_alter_column_table_registeredcoop.php
+++ application/migrations/127_alter_column_table_registeredcoop.php
@@ -3,7 +3,8 @@ class Migration_alter_column_table_registeredcoop extends CI_Migration
 {
     public function up()
     {
-       $this->db->query("ALTER TABLE registeredcoop RENAME COLUMN dateRegistered TO date_printed");
+       $this->db->query("ALTER TABLE `registeredcoop` 
+CHANGE COLUMN `dateRegistered` `date_printed` VARCHAR(10) NOT NULL");
        $this->db->query("ALTER TABLE registeredcoop ADD COLUMN dateRegistered VARCHAR(10) DEFAULT NULL AFTER date_printed");
 
         $this->db->query("UPDATE registeredcoop JOIN cooperatives ON registeredcoop.application_id = cooperatives.id SET registeredcoop.dateRegistered = cooperatives.date_of_or");
@@ -13,7 +14,7 @@ class Migration_alter_column_table_registeredcoop extends CI_Migration
     public function down()
     {
          $this->db->query("ALTER TABLE registeredcoop DROP COLUMN dateRegistered");
-        $this->db->query("ALTER TABLE registeredcoop RENAME COLUMN date_printed TO dateRegistered ");
+        $this->db->query("ALTER TABLE registeredcoop CHANGE COLUMN date_printed TO dateRegistered ");
        
     }
 }
