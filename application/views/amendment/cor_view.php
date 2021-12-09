142791290a39d0a5d97bf0a62e758662255046c1
jaysondelapaz16@gmail.com <jaysondelapaz16@gmail.com>
2021-12-09 16:17:39 +0800

    update COR amendment

--- application/views/amendment/cor_view.php
+++ application/views/amendment/cor_view.php
@@ -21,44 +21,7 @@
   </style>
 </head>
 <body>  
-
-	<div  style="height: 1060px;border: 10px black double; border-radius: 30px; padding: 20px">
-	<table width=100%>
-		<tr>
-		<!-- 	<td><img src="<?=APPPATH?>../assets/img/cda4.png" width="100" height="100" ></td> -->
-			<td width="7%"><i style="color:white;">....</i></td>
-			<td width="15%"><img src="<?=APPPATH?>../assets/img/cda_new.jpg" width="100" height="100" ></td>
-			<td style="text-align: center"><b>Republic of the Philippines<br/>COOPERATIVE DEVELOPMENT AUTHORITY</b></td>
-			<td width="18%"><i style="color:white;">....</i></td>
-		</tr>
-	</table>
-<?php if($coop_info->noStreet==null && $coop_info->Street==null) $x=''; else $x=', ';?>
-<br/><br/>
-	<table width=100%  style="background: url(<?=base_url();?>/assets/img/cda4.png); background-repeat: no-repeat; background-position: center;margin-top:-30px;padding-bottom: 60px;opacity: 0.2;">
-		<tr>
-			
-			<td style="text-align: right; font-size: 12pt"><b>Registration No: <?= $coop_info->regNo?></b></td>
-		</tr>
-	        <tr>
-			<td style="text-align: right; font-size: 12pt"><b>Amendment No: <?= $coop_info->regNo.'-'.$coop_info->amendmentNo ?></b></td>
-		</tr>
-		<tr>
-			<td><i style="color:white; font-size: 8pt">....</i></td>
-		</tr>
-		<tr>
-	            <td style="color: white; background-color: #003399; text-align: center; font-size: 15pt">CERTIFICATE OF REGISTRATION<br>OF<br>AMENDMENTS TO THE<br>ARTICLES OF COOPERATION AND BY LAWS</td>
-		</tr>
-		<tr>
-			<td><i style="color:white; font-size: 8pt">....</i></td>
-		</tr>
-		<tr>
-			<td style="font-weight: bold; font-size: 10pt">KNOW ALL MEN BY THESE PRESENTS, GREETINGS:<br/></td>
-		</tr>
-	        <tr>
-			<td><i style="color:white;">....</i></td>
-		</tr>
-		<tr>
-			<?php
+	<?php
 				$artilces_array = array($typeOfcoop,
 									    $proposeName,
 									    $acronym_c,
@@ -132,6 +95,117 @@
 						$and2=' and ';
 					}
 			?>
+	<div  style="height: 1060px;border: 10px black double; border-radius: 30px; padding: 20px">
+	<table width=100%>
+		<tr>
+		<!-- 	<td><img src="<?=APPPATH?>../assets/img/cda4.png" width="100" height="100" ></td> -->
+			<td width="7%"><i style="color:white;">....</i></td>
+			<td width="15%"><img src="<?=APPPATH?>../assets/img/cda_new.jpg" width="100" height="100" ></td>
+			<td style="text-align: center"><b>Republic of the Philippines<br/>COOPERATIVE DEVELOPMENT AUTHORITY</b></td>
+			<td width="18%"><i style="color:white;">....</i></td>
+		</tr>
+	</table>
+<?php if($coop_info->noStreet==null && $coop_info->Street==null) $x=''; else $x=', ';?>
+<br/><br/>
+	<table width=100%  style="background: url(<?=base_url();?>/assets/img/cda4.png); background-repeat: no-repeat; background-position: center;margin-top:-30px;padding-bottom: 60px;opacity: 0.2;">
+		<tr>
+			
+			<td style="text-align: right; font-size: 12pt"><b>Registration No: <?= $coop_info->regNo?></b></td>
+		</tr>
+	        <tr>
+			<td style="text-align: right; font-size: 12pt"><b>Amendment No: <?= $coop_info->regNo.'-'.$coop_info->amendmentNo ?></b></td>
+		</tr>
+		<tr>
+			<td><i style="color:white; font-size: 8pt">....</i></td>
+		</tr>
+		<tr>
+	            <td style="color: white; background-color: #003399; text-align: center; font-size: 15pt">CERTIFICATE OF REGISTRATION<br>OF<br>AMENDMENTS TO THE<br>
+	            	<?php if($articles){?><b>ARTICLES OF COOPERATION</b><?php }?><?=$and;?><?php if($bylaws){?><b>BY LAWS</b><?php }?></td>
+		</tr>
+		<tr>
+			<td><i style="color:white; font-size: 8pt">....</i></td>
+		</tr>
+		<tr>
+			<td style="font-weight: bold; font-size: 10pt">KNOW ALL MEN BY THESE PRESENTS, GREETINGS:<br/></td>
+		</tr>
+	        <tr>
+			<td><i style="color:white;">....</i></td>
+		</tr>
+		<tr>
+<!-- 			<?php
+				$artilces_array = array($typeOfcoop,
+									    $proposeName,
+									    $acronym_c,
+									    $common_bond,
+									    $areaOf_operation,
+									    $fieldOfmemship,
+									    $address1,
+									    $authorized_share_capital,
+											$common_share,
+											$preferred_share,
+											$par_value,
+											$authorized_share_capital,
+											$total_amount_of_subscribed_capital,
+											// $amount_of_common_share_subscribed,
+											$amount_of_preferred_share_subscribed,
+											$total_amount_of_paid_up_capital,
+											// $amount_of_common_share_paidup,
+											$amount_of_preferred_share_paidup,
+											$purposes,
+											$no_of_bod
+							       );
+				// echo "<pre>";print_r($artilces_array);echo"</pre>";
+				$bylaws_array = array(
+							      $no_of_bod,
+							      $kinds_of_members,
+					          $additional_requirements_for_membership,
+					          $regular_qualifications,
+					          $associate_qualifications, 
+					          $membership_fee,
+					          $act_upon_membership_days,
+					          $additional_conditions_to_vote,  
+					          $annual_regular_meeting_day, 
+					          // $annual_regular_meeting_day_date,
+					          // $annual_regular_meeting_day_venue, 
+					          $members_percent_quorom,
+					          $number_of_absences_disqualification,
+					          $percent_of_absences_all_meettings, 
+					          $director_hold_term,
+					          $member_invest_per_month, 
+					          $member_percentage_annual_interest, 
+					          $member_percentage_service, 
+					          $percent_reserve_fund, 
+					          $percent_education_fund, 
+					          $percent_community_fund,
+					          $percent_optional_fund, 
+					          $non_member_patron_years, 
+					          $amendment_votes_members_with,
+					          $minimum_subscribed_share_regular, 
+					          $minimum_paid_up_share_regular, 
+					          $minimum_subscribed_share_associate,
+					          $minimum_paid_up_share_associate,
+					          $committees_others 
+				);
+				// echo "<pre>";print_r($bylaws_array);echo"</pre>";
+				$and = '';
+				$and2='';
+				$bylaws = false;
+				$articles = false;
+					if(in_array('true',$bylaws_array))
+					{
+						$bylaws = true;
+					}
+					if(in_array('true',$artilces_array))
+					{
+						$articles = true;
+					}
+				
+					if($articles && $bylaws)
+					{
+						$and=' AND ';
+						$and2=' and ';
+					}
+			?> -->
 
 			<?php
 
