bdfdd95f219dbdc79bb553a7fcc92b1d9912e516
Anjury Patawaran <anjurypatawaran@gmail.com>
2022-02-15 17:49:59 +0800

    update affiliators model 4

--- application/models/Affiliators_model.php
+++ application/models/Affiliators_model.php
@@ -185,15 +185,15 @@ class Affiliators_model extends CI_Model{
     }
     
     public function get_applied_coop($user_id){
-        $this->db->select("affiliators.*, affiliators.id AS aff_id, registeredcoop.*, registeredcoop.id as registered_id, cooperatives.*, refbrgy.brgyDesc as brgy, refcitymun.citymunDesc as city, refprovince.provDesc as province, refregion.regDesc as region");
-        $this->db->from('affiliators');
-        $this->db->join('cooperatives', 'affiliators.application_id = cooperatives.id');
-        $this->db->join('registeredcoop','registeredcoop.id = affiliators.registeredcoop_id');
-        $this->db->join('refbrgy' , 'refbrgy.brgyCode = registeredcoop.addrCode');
-        $this->db->join('refcitymun', 'refcitymun.citymunCode = refbrgy.citymunCode');
-        $this->db->join('refprovince', 'refprovince.provCode = refcitymun.provCode');
-        $this->db->join('refregion', 'refregion.regCode = refprovince.regCode');
-        $this->db->where('affiliators.user_id ='.$user_id);
+        $this->db->select('affiliators.*, affiliators.id AS aff_id, registeredcoop.*, registeredcoop.id as registered_id, cooperatives.*, refbrgy.brgyDesc as brgy, refcitymun.citymunDesc as city, refprovince.provDesc as province, refregion.regDesc as region');
+        // $this->db->from('affiliators');
+        $this->db->join('cooperatives', 'affiliators.application_id = cooperatives.id','left');
+        $this->db->join('registeredcoop','registeredcoop.id = affiliators.registeredcoop_id','right');
+        $this->db->join('refbrgy' , 'refbrgy.brgyCode = registeredcoop.addrCode','left');
+        $this->db->join('refcitymun', 'refcitymun.citymunCode = refbrgy.citymunCode','left');
+        $this->db->join('refprovince', 'refprovince.provCode = refcitymun.provCode','left');
+        $this->db->join('refregion', 'refregion.regCode = refprovince.regCode','left');
+        $this->db->where('user_id ='.$user_id);
         $query = $this->db->get();
         $data = $query->result_array();
         return $data;
