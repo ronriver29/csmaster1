d603d57452649d1c76f65ab9749dfc586f73789a
Anjury Patawaran <anjurypatawaran@gmail.com>
2022-03-21 15:44:14 +0800

    sign up remove on new account

--- application/views/client/create_new_email_account.php
+++ application/views/client/create_new_email_account.php
@@ -229,9 +229,9 @@
               <div class="col-md-6">
                 <a class="btn btn-link" href="<?php echo base_url();?>users/login" role="button">Sign In Instead</a>
               </div>
-              <div class="col-md-6">
+              <!-- <div class="col-md-6">
                 <a class="btn btn-link" href="<?php echo base_url();?>users/use_registered_email" role="button">Use Registered Email Account</a>
-              </div>
+              </div> -->
             </div>
           </div>
           <div class="col-sm-12 offset-md-8 col-md-2 align-self-center order-xs-2 order-sm-2 order-1 col-signup-btn">
