951b3c796aeeaae9f4a54512b35af5d86e07725a
Anjury Patawaran <anjurypatawaran@gmail.com>
2022-03-21 10:38:54 +0800

    added models on amendment

---
+++ application/models/Amendment_update_bylaw_model.php
@@ -0,0 +1,232 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Amendment_update_bylaw_model extends CI_Model{
+
+  public function __construct()
+  {
+    parent::__construct();
+    //Codeigniter : Write Less Do More
+    $this->load->database();
+  }
+  public function get_bylaw_by_coop_id($coop_id,$amendment_id){
+    $data_coop_id = $this->security->xss_clean($coop_id);
+    $data_amendment_id = $this->security->xss_clean($amendment_id);
+    $query = $this->db->get_where('amendment_bylaws',array('amendment_id'=>$data_amendment_id,'cooperatives_id'=>$data_coop_id));
+    if($query->num_rows()==1)
+    {
+      $data = $query->row();
+    }
+    else
+    {
+      $data = null;
+    }
+    return $data;
+  }
+  public function update_bylaw_primary($bylaw_coop_id,$bylaw_info){
+    $bylaw_coop_id = $this->security->xss_clean($bylaw_coop_id);
+    $bylaw_info = $this->security->xss_clean($bylaw_info);
+   
+    /*check record first if existing if not then create*/
+    $get_record = $this->db->where("amendment_id",$bylaw_coop_id)->get("amendment_bylaws");
+    if($get_record->num_rows()==0) {
+        $this->db->insert('amendment_bylaws', array('amendment_id'=>$bylaw_coop_id));
+        $this->db->trans_commit();
+    }
+    $this->db->trans_begin();
+
+    $this->db->where('amendment_id', $bylaw_coop_id);
+    $this->db->update('amendment_bylaws',$bylaw_info);
+    if( $bylaw_info['kinds_of_members'] ==1)
+    {
+      $this->db->delete('amendment_cooperators',array('amendment_id'=>$bylaw_coop_id,'type_of_member'=>'Associate'));
+      $this->db->where('amendment_id',$bylaw_coop_id);
+      $this->db->update('amendment_capitalization',array('associate_members'=>''));
+    }
+    if($this->db->trans_status() === FALSE){
+      $this->db->trans_rollback();
+      return false;
+    }else{
+      $this->db->trans_commit();
+      return true;
+    }
+  }
+  public function check_bylaw_primary_complete($bylaw_coop_id,$amendment_id){
+    $data = array();
+    $counter = 0;
+    $query = $this->db->get_where('amendment_bylaws',array('amendment_id'=>$amendment_id,'cooperatives_id'=>$bylaw_coop_id));
+    if($query->num_rows()>0) {
+        $data = $query->row();
+    }
+    if(isset($data->id)) {
+        foreach ($data as $key => $value){
+          if(empty($value)) $counter++;
+        }
+        if($data->kinds_of_members==1){
+          if($counter<=14){
+            if($this->ga_venue($amendment_id))
+            {
+              return true;
+            }
+            
+          }else{
+            return false;
+          }
+        }else{
+          if($counter<=14){
+            if($this->ga_venue($amendment_id))
+            {
+              return true;
+            }
+            
+          }else{
+            return false;
+          }
+        }
+    } else {
+        return false;
+    }
+  }
+
+  //check if there is GA Venue
+  public function ga_venue($amendment_id)
+  {
+    
+    $qry =$this->db->query("select annual_regular_meeting_day_venue from amendment_bylaws where amendment_id='$amendment_id'");
+    if($qry->num_rows()>0)
+    {
+      foreach($qry->result_array() as $row)
+      {
+        $venue = $row['annual_regular_meeting_day_venue'];
+      }
+      if(strlen($venue)>0)
+      {
+        $data=true;
+      }
+      else
+      {
+        $data=false;
+      }
+    }
+    else
+    {
+      $data= false;
+    }
+    return $data; 
+  }
+  // end GA venue
+
+  public function check_minimum_regular_subscription_amendment($ajax){
+    $cooperative_id = $this->encryption->decrypt(decrypt_custom($ajax['coop_id']));
+    $amendment_id = $this->encryption->decrypt(decrypt_custom($ajax['amendment_id']));
+    $this->db->select('amendment_bylaws.regular_percentage_shares_subscription');
+    $this->db->from('amendment_bylaws');
+    // $this->db->join('amend_coop','amend_coop.id = amendment_bylaws.cooperatives_id','inner');
+    $this->db->where(array('cooperatives_id'=>$cooperative_id,'amendment_id'=>$amendment_id));
+    $query = $this->db->get();
+    $data = $query->row();return $data;
+    // if($data->regular_percentage_shares_subscription <= $ajax['fieldValue']){
+    //   return array($ajax['fieldId'],true);
+    // }else{
+    //   return array($ajax['fieldId'],false);
+    // }
+  }
+  public function check_minimum_regular_subscription($ajax){
+    $decoded_id = $this->encryption->decrypt(decrypt_custom($ajax['coop_id']));
+    $this->db->select('amendment_bylaws.regular_percentage_shares_subscription');
+    $this->db->from('amendment_bylaws');
+    $this->db->join('cooperatives','cooperatives.id = amendment_bylaws.cooperatives_id','inner');
+    $this->db->where(array('cooperatives_id'=>$decoded_id));
+    $query = $this->db->get();
+    $data = $query->row();
+    if($data->regular_percentage_shares_subscription <= $ajax['fieldValue']){
+      return array($ajax['fieldId'],true);
+    }else{
+      return array($ajax['fieldId'],false);
+    }
+  }
+  public function check_minimum_regular_pay_amendment($ajax){
+    $decoded_id = $this->encryption->decrypt(decrypt_custom($ajax['coop_id']));
+    $this->db->select('amendment_bylaws.regular_percentage_shares_pay');
+    $this->db->from('amendment_bylaws');
+    $this->db->join('amend_coop','amend_coop.id = amendment_bylaws.cooperatives_id','inner');
+    $this->db->where(array('cooperatives_id'=>$decoded_id));
+    $query = $this->db->get();
+    $data = $query->row();
+    if($data->regular_percentage_shares_pay<= $ajax['fieldValue']){
+      return array($ajax['fieldId'],true);
+    }else{
+      return array($ajax['fieldId'],false);
+    }
+  }
+  public function check_minimum_regular_pay($ajax){
+    $decoded_id = $this->encryption->decrypt(decrypt_custom($ajax['coop_id']));
+    $this->db->select('amendment_bylaws.regular_percentage_shares_pay');
+    $this->db->from('amendment_bylaws');
+    $this->db->join('cooperatives','cooperatives.id = amendment_bylaws.cooperatives_id','inner');
+    $this->db->where(array('cooperatives_id'=>$decoded_id));
+    $query = $this->db->get();
+    $data = $query->row();
+    if($data->regular_percentage_shares_pay<= $ajax['fieldValue']){
+      return array($ajax['fieldId'],true);
+    }else{
+      return array($ajax['fieldId'],false);
+    }
+  }
+  public function check_minimum_associate_subscription($ajax){
+    $decoded_id = $this->encryption->decrypt(decrypt_custom($ajax['coop_id']));
+    $this->db->select('amendment_bylaws.associate_percentage_shares_subscription');
+    $this->db->from('amendment_bylaws');
+    $this->db->join('cooperatives','cooperatives.id = amendment_bylaws.cooperatives_id','inner');
+    $this->db->where(array('cooperatives_id'=>$decoded_id));
+    $query = $this->db->get();
+    $data = $query->row();
+    if($data->associate_percentage_shares_subscription <= $ajax['fieldValue']){
+      return array($ajax['fieldId'],true);
+    }else{
+      return array($ajax['fieldId'],false);
+    }
+  }
+  public function check_minimum_associate_subscription_amendment($ajax){
+    $decoded_id = $this->encryption->decrypt(decrypt_custom($ajax['coop_id']));
+    $this->db->select('amendment_bylaws.associate_percentage_shares_subscription');
+    $this->db->from('amendment_bylaws');
+    $this->db->join('amend_coop','amend_coop.id = amendment_bylaws.cooperatives_id','inner');
+    $this->db->where(array('cooperatives_id'=>$decoded_id));
+    $query = $this->db->get();
+    $data = $query->row();
+    if($data->associate_percentage_shares_subscription <= $ajax['fieldValue']){
+      return array($ajax['fieldId'],true);
+    }else{
+      return array($ajax['fieldId'],false);
+    }
+  }
+  public function check_minimum_associate_pay($ajax){
+   
+    $this->db->select('associate_percentage_shares_pay');
+    $this->db->from('amendment_bylaws');
+    // $this->db->join('cooperatives','cooperatives.id = amendment_bylaws.cooperatives_id','inner');
+    $this->db->where(array('cooperatives_id'=>$ajax['cooperative_id'],'amendment_id'=>$ajax['amendment_id'],'id'=>$ajax['cooperatorID']));
+    $query = $this->db->get();
+    $data = $query->row();
+    if($data->associate_percentage_shares_pay<= $ajax['fieldValue']){
+      return array($ajax['fieldId'],true);
+    }else{
+      return array($ajax['fieldId'],false);
+    }
+  }
+  public function update_bylaw_union($bylaw_coop_id,$bylaw_info){
+    $bylaw_id = $this->security->xss_clean($bylaw_coop_id);
+    $bylaw_info = $this->security->xss_clean($bylaw_info);
+    $this->db->trans_begin();
+    $this->db->where('cooperatives_id', $bylaw_coop_id);
+    $this->db->update('amendment_bylaws',$bylaw_info);
+    if($this->db->trans_status() === FALSE){
+      $this->db->trans_rollback();
+      return false;
+    }else{
+      $this->db->trans_commit();
+      return true;
+    }
+  }
+}
---
+++ application/models/Amendment_update_capitalization_model.php
@@ -0,0 +1,155 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Amendment_update_capitalization_model extends CI_Model{
+
+  public function __construct()
+  {
+    parent::__construct();
+    //Codeigniter : Write Less Do More
+//    $this->load->database();
+  }
+  public function get_capitalization_by_coop_id($coop_id,$amendment_id){
+    $data_coop_id = $this->security->xss_clean($coop_id);
+     $data_amendment_id = $this->security->xss_clean($amendment_id);
+    $query = $this->db->get_where('amendment_capitalization',array('cooperatives_id'=>$data_coop_id,'amendment_id'=>$data_amendment_id));
+    return $query->row();
+  }
+  public function amend_get_capitalization_by_coop_id($coop_id){
+    $data = $this->security->xss_clean($coop_id);
+    $query = $this->db->get_where('amendment_capitalization',array('cooperatives_id'=>$data));
+    return $query->row();
+  }
+  public function amend_get_capitalization_by_coop_id_count($coop_id){
+    $data = $this->security->xss_clean($coop_id);
+    $query = $this->db->get_where('amendment_capitalization',array('cooperatives_id'=>$data));
+    return $query->num_rows();
+  }
+
+  public function update_capitalization($amendment_id,$capitalization_coop_id,$capitalization_info){
+      $capitalization_coop_id = $this->security->xss_clean($capitalization_coop_id);
+      $capitalization_info = $this->security->xss_clean($capitalization_info);
+        $amendment_id = $this->security->xss_clean($amendment_id);
+      $query= $this->db->get_where('amendment_capitalization',array('cooperatives_id'=>$capitalization_coop_id,'amendment_id'=>$amendment_id));
+      if($query->num_rows()>0)
+       {
+        $this->db->trans_begin();
+        $this->db->where('cooperatives_id', $capitalization_coop_id);
+        $this->db->where('amendment_id',$amendment_id);
+        $this->db->update('amendment_capitalization',$capitalization_info);
+      } 
+      else
+      {
+      	$capitalization_info['cooperatives_id'] = $capitalization_coop_id;
+      	$capitalization_info['amendment_id'] = $amendment_id;
+      	$this->db->insert('amendment_capitalization',$capitalization_info);
+      }
+      // return $capitalization_info;
+        if($this->db->trans_status() === FALSE){
+          $this->db->trans_rollback();
+          return false;
+        }else{
+          $this->db->trans_commit();
+          return true;
+        }
+  }
+  public function check_capitalization_primary_complete($capitalization_coop_id,$amendment_id){
+    $counter = 0;
+    $query_bylaws = $this->db->get_where('amendment_bylaws',array('cooperatives_id'=>$capitalization_coop_id,'amendment_id'=>$amendment_id));
+    $data_bylaws = $query_bylaws->row();
+    $query = $this->db->get_where('amendment_capitalization',array('cooperatives_id'=>$capitalization_coop_id,'amendment_id'=>$amendment_id));
+    if($query->num_rows()>0) {
+     $data = $query->row();
+        $required_fields = array(
+            "regular_members",
+            "authorized_share_capital",
+            "par_value",
+            "common_share",
+            "total_amount_of_subscribed_capital",
+            "total_no_of_subscribed_capital",
+            "total_amount_of_paid_up_capital",
+            "total_no_of_paid_up_capital",
+            "minimum_subscribed_share_regular",
+            "minimum_paid_up_share_regular",
+            );
+        if($data_bylaws->kinds_of_members==2){
+            $required_fields[] = "associate_members";
+            $required_fields[] = "preferred_share";
+            $required_fields[] = "minimum_subscribed_share_associate";
+            $required_fields[] = "minimum_paid_up_share_associate";
+        }
+        foreach ($required_fields as $field){
+          if($data->$field=="") $counter++;
+        }
+        if($counter==0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+  }
+ 
+  public function check_minimum_regular_subscription($ajax){
+    $decoded_id = $this->encryption->decrypt(decrypt_custom($ajax['coop_id']));
+    $amendment_id = $this->encryption->decrypt(decrypt_custom($ajax['amendment_id']));
+    $this->db->select('minimum_subscribed_share_regular,total_no_of_subscribed_capital');
+    $this->db->from('amendment_capitalization');
+    // $this->db->join('cooperatives','cooperatives.id = capitalization.cooperatives_id','inner');
+    $this->db->where(array('cooperatives_id'=>$decoded_id,'amendment_id'=>$amendment_id));
+    $query = $this->db->get();
+    $data = $query->row();
+    if($data->minimum_subscribed_share_regular <= $ajax['fieldValue'] && $data->total_no_of_subscribed_capital * 0.10 >= $ajax['fieldValue']){
+      return array($ajax['fieldId'],true);
+    }else{
+      return array($ajax['fieldId'],false);
+    }
+  }
+
+  public function check_minimum_regular_pay($ajax){
+    $decoded_id = $this->encryption->decrypt(decrypt_custom($ajax['amendment_id']));
+    $this->db->select('amendment_capitalization.minimum_paid_up_share_regular');
+    $this->db->from('amendment_capitalization');
+    // $this->db->join('cooperatives','cooperatives.id = capitalization.cooperatives_id','inner');
+    $this->db->where(array('amendment_id'=>$decoded_id));
+    $query = $this->db->get();
+    $data = $query->row();
+    if($data->minimum_paid_up_share_regular<= $ajax['fieldValue']){
+      return array($ajax['fieldId'],true);
+    }else{
+      return array($ajax['fieldId'],false);
+    }
+  }
+  public function check_minimum_associate_subscription($ajax){
+    // $decoded_id = $this->encryption->decrypt(decrypt_custom($ajax['coop_id']));
+    $this->db->select('minimum_subscribed_share_associate,total_no_of_subscribed_capital');
+    $this->db->from('amendment_capitalization');
+    // $this->db->join('cooperatives','cooperatives.id = capitalization.cooperatives_id','inner');
+    $this->db->where(array('cooperatives_id'=>$ajax['cooperative_id'],'amendment_id'=>$ajax['amendment_id']));
+    $query = $this->db->get();
+    // return $this->db->last_query();
+    $data = $query->row();
+
+    if($data->minimum_subscribed_share_associate <= $ajax['fieldValue'] && $data->total_no_of_subscribed_capital*0.10 >= $ajax['fieldValue']) {
+      return  array($ajax['fieldId'],true);
+    }else{
+      return array($ajax['fieldId'],false);
+    }
+  }
+  public function check_minimum_associate_pay($ajax){
+    $this->db->select('minimum_paid_up_share_associate');
+    $this->db->from('amendment_capitalization');
+    // $this->db->join('cooperatives','cooperatives.id = capitalization.cooperatives_id','inner');
+    $this->db->where(array('cooperatives_id'=>$ajax['cooperative_id'],'amendment_id'=>$ajax['amendment_id']));
+    $query = $this->db->get();
+    $data = $query->row();
+    if($data->minimum_paid_up_share_associate<= $ajax['fieldValue']){
+      return array($ajax['fieldId'],true);
+    }else{
+      return array($ajax['fieldId'],false);
+    }
+  }
+  
+}
