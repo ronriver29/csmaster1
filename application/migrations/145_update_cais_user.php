e1709e7c867cf30ddbd34a41b4e9d46840dca61b
jaysondelapaz <jaysondelapaz16@gmail.com>
2022-05-18 17:46:38 +0800

    fix migration erro

--- application/migrations/145_update_cais_user.php
+++ application/migrations/145_update_cais_user.php
@@ -4,7 +4,7 @@ class Migration_update_cais_user extends CI_Migration
     
     public function up()
     {
-        $this->db->query("UPDATE ca_user SET regNo = REPLACE(regNo, '9520','9520-') WHERE LOCATE("-", regNo) = 0;");
+        $this->db->query("UPDATE ca_user SET regNo = REPLACE(regNo, '9520','9520-') WHERE LOCATE('-', regNo) = 0;");
     }
     
     public function down()
