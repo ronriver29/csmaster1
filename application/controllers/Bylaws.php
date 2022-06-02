259e3cb078a2de99d82526dc7bf28f6ef6718881
Anjury Patawaran <anjurypatawaran@gmail.com>
2022-06-02 16:48:41 +0800

    bylaws update

--- application/controllers/Bylaws.php
+++ application/controllers/Bylaws.php
@@ -106,14 +106,14 @@ class Bylaws extends CI_Controller{
                         }
                       $regQualicationsTemp = substr_replace($regQualicationsTemp, "", -1);
                     }
-                    // if($this->input->post('associateQualifications') != ''){
-                    //   $ascQualificationsLength = sizeof($this->input->post('associateQualifications'));
-                    //   $ascQualicationsTemp = '';
-                    //   for($i = 0; $i< $ascQualificationsLength;$i++){
-                    //       $ascQualicationsTemp .=  $this->input->post('associateQualifications')[$i].';';
-                    //   }
-                    //   $ascQualicationsTemp = substr_replace($ascQualicationsTemp, "", -1);
-                    // }
+                    if($this->input->post('associateQualifications')){
+                      $ascQualificationsLength = sizeof($this->input->post('associateQualifications'));
+                      $ascQualicationsTemp = '';
+                      for($i = 0; $i< $ascQualificationsLength;$i++){
+                          $ascQualicationsTemp .=  $this->input->post('associateQualifications')[$i].';';
+                      }
+                      $ascQualicationsTemp = substr_replace($ascQualicationsTemp, "", -1);
+                    }
 
                     $membership_fee=str_replace(',','',$this->input->post('membershipFee'));
                     $investPerMonth=str_replace(',','',$this->input->post('investPerMonth'));
@@ -618,7 +618,7 @@ class Bylaws extends CI_Controller{
                       $additionalConditionsForVoting = substr_replace($additionalConditionsForVoting, "", -1);
                     }
                      $ascQualicationsTemp = '';
-                    if($this->input->post('associateQualifications')){
+                    if($this->input->post('associateQualifications') != ''){
                       $ascQualificationsLength = sizeof($this->input->post('associateQualifications'));
                      
                       for($i = 0; $i< $ascQualificationsLength;$i++){
