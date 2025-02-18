b5fae405aa4d6139b4142b97f1d189356daeab49
Romaine Niño P. Talucod <r_talucod@cda.gov.ph>
2025-02-18 05:07:31 +0000

    Update file cor_view.php

--- application/views/amendment/cor_view.php
+++ application/views/amendment/cor_view.php
@@ -246,7 +246,11 @@
 
 			<td  style="text-align: center;padding-left:150px;"> 
 
+			<div class="text" style="margin-top:-75px;width:400px;padding-left: 140px;">
+
 			<img src="<?=APPPATH.$signature?>" style="width:270px;height:100px;padding-left:140px;">
+
+			</div>
 
 			<div class="text" style="margin-top:-75px;width:400px;padding-left: 140px;">
 
